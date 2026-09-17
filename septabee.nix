3697fd21fd629816dd06376166675a99a34c6909
JuxGD <jak@e.email>
2026-09-17 14:30:24 -0300

    B_T2

--- septabee.nix
+++ septabee.nix
@@ -5,10 +5,10 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "septabee";
-  version = "B_T1";
+  version = "B_T2";
   src = pkgs.fetchurl {
     url = "https://septabee.nekoweb.org/important_stuff/SEPTABEE_DOWNLOADS/version_B/septabee_linux_${version}.7z";
-    hash = "sha256-JlWmeDnMTjBNwLTADvSswbtfhJK6t1bu0xHkmBgLtvA=";
+    hash = "sha256-OMnbRBTku8yi4b3Ay7d70EbB/e2Qh+PfzK2O8qRFoaA=";
   };
 
   # from here on out. thanks Ap6661/septabee-flake contributors
