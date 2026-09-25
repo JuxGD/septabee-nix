71f539d3b1ae976f4ef3270be3aeb4c33cd6e548
JuxGD <jak@e.email>
2026-09-25 15:05:09 -0300

    oops i broke it accidentally

--- default.nix
+++ default.nix
@@ -1,7 +1,5 @@
 { pkgs ? import <nixpkgs> { }, waylandSupport ? true, offline ? true }:
 
-let module = import ./module.nix;
-
 rec {
   septabee = pkgs.callPackage ./septabee.nix { inherit waylandSupport; inherit offline; };
 
